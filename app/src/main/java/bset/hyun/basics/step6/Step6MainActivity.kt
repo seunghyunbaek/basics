aa4df3571e4b0fec436dea0083247708a8bdfb3e
SeungHyunBaek <back947@naver.com>
2020-07-21 23:58:04 +0900

    Learn step6, Database, SQL문 기초

---
+++ app/src/main/java/bset/hyun/basics/step6/Step6MainActivity.kt
@@ -0,0 +1,13 @@
+package bset.hyun.basics.step6
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import bset.hyun.basics.R
+
+class Step6MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_step6_main)
+    }
+}
